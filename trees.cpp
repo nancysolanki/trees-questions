acf2f06e87995a0a414972d2eccae93d0e20818a
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-04 17:39:42 +0530

    Update trees.cpp

--- trees.cpp
+++ trees.cpp
@@ -1,1 +1,15 @@
-
+  
+struct Node { 
+    int data; 
+    struct Node* left; 
+    struct Node* right; 
+  
+    Node(int val) 
+    { 
+        data = val; 
+  
+        left = NULL; 
+        right = NULL; 
+    } 
+}; 
+  
